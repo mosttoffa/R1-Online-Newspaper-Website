4ec3e4fd640a1a73ce9dacd166d6894d90808d67
mosttoffa <mostofacba@gmail.com>
2021-04-03 14:02:12 +0600

    Create app "Main"

--- myproject/urls.py
+++ myproject/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     
     url(r'^admin/' , admin.site.urls),
-    # url(r'', include('main.urls')),
+    url(r'', include('main.urls')),
 ]
 
 if settings.DEBUG:
